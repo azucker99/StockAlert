c7afc1904920fcd0ab08121f83ba15eb89d27b34
Adam Zucker <adamzucker@adams-mbp.mynetworksettings.com>
2024-07-14 11:00:31 -0400

    Add foundation for CDK project.

---
+++ app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from __future__ import annotations
+
+import aws_cdk as cdk
+
+
+app = cdk.App()
+
+app.synth()
---
+++ stock_alert/stock_alert_stack.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from aws_cdk import (
+    # Duration,
+    Stack,
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+
+class StockAlertStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "StockAlertQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
